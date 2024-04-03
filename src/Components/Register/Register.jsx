46cb237d39498d6e0246de3c942658f1b52a9203
asraful613 <atifasraful530@gmail.com>
2024-04-03 17:59:33 +0100

    added a password validation

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,15 +1,25 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../AuthProvider/AuthProvider";
 
 const Register = () => {
     const {register}=useContext(AuthContext)
+    const [error,setError]=useState('')
     const handleRegisterForm=e=>{
+      setError('')
         e.preventDefault()
      const name=e.target.name.value;
      const photo=e.target.photo.value;
      const email=e.target.email.value;
      const password=e.target.password.value;
      const confirmPassword=e.target.confirmPassword.value;
+     if(password.length<6){
+       setError('Password must be at least 6 characters')
+       return
+     }
+     if(password!==confirmPassword){
+       setError('Passwords do not match')
+        return
+     }
         console.log(name,photo,email,password,confirmPassword);
      register(email,password)
     }
@@ -49,6 +59,9 @@ const Register = () => {
             <span className="label-text">Confirm Password</span>
           </label>
           <input name="confirmPassword" type="confirm-password" placeholder="confirmPassword" className="input input-bordered" required />
+          {
+            error && <small>{error}</small>
+          }
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
